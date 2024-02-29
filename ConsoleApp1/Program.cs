e3efa6b977d80b17319c215c68d6ad09c04073d2
AleksandraKazmierska <olaa.kazmierska@student.put.poznan.pl>
2024-02-29 10:34:47 +0100

    zmiany 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("hELLO WORLD");
-
+Console.WriteLine("added line");
+Console.WriteLine("zmiana");
